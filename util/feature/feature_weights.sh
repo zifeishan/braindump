65fc27ecd49b8e4538f612cb72f59bb916497363
Zifei Shan <zifei@stanford.edu>
2015-01-01 23:27:30 -0800

    Made tsv header compatible; put description last for better layout

--- util/feature/feature_weights.sh
+++ util/feature/feature_weights.sh
@@ -6,34 +6,39 @@ psql -d $DBNAME -c "\d dd_feature_statistics" >/dev/null
 if [ $? -eq 0 ]; then
 
   # Sample features with #pos/neg examples
+  printf "weight\tpos_examples\tneg_examples\tqueries\tdescription\n" >$OUTPUT_DIR/positive_features.tsv
   psql -d $DBNAME -c "COPY (
-    SELECT description, weight, pos_examples, neg_examples, queries
+    SELECT weight, pos_examples, neg_examples, queries, description
     FROM dd_feature_statistics 
     ORDER BY weight DESC 
     LIMIT ${NUM_SAMPLED_FEATURES}
-  ) TO STDOUT HEADER;" > $OUTPUT_DIR/positive_features.tsv
+  ) TO STDOUT;" >> $OUTPUT_DIR/positive_features.tsv
 
+  printf "weight\tpos_examples\tneg_examples\tqueries\tdescription\n" >$OUTPUT_DIR/negative_features.tsv
   psql -d $DBNAME -c "COPY (
-    SELECT description, weight, pos_examples, neg_examples, queries
+    SELECT weight, pos_examples, neg_examples, queries, description
     FROM dd_feature_statistics 
     ORDER BY weight ASC 
     LIMIT ${NUM_SAMPLED_FEATURES}
-  ) TO STDOUT HEADER;" > $OUTPUT_DIR/negative_features.tsv
+  ) TO STDOUT;" >> $OUTPUT_DIR/negative_features.tsv
 
 else
 
+  printf "weight\tdescription\n" >$OUTPUT_DIR/positive_features.tsv
   psql -d $DBNAME -c "COPY (
-    SELECT description, weight
+    SELECT weight, description
     FROM dd_inference_result_variables_mapped_weights 
     ORDER BY weight DESC 
     LIMIT ${NUM_SAMPLED_FEATURES}
-  ) TO STDOUT HEADER;" > $OUTPUT_DIR/positive_features.tsv
+  ) TO STDOUT;" >> $OUTPUT_DIR/positive_features.tsv
 
+
+  printf "weight\tdescription\n" >$OUTPUT_DIR/negative_features.tsv
   psql -d $DBNAME -c "COPY (
-    SELECT description, weight
+    SELECT weight, description
     FROM dd_inference_result_variables_mapped_weights 
     ORDER BY weight ASC 
     LIMIT ${NUM_SAMPLED_FEATURES}
-  ) TO STDOUT HEADER;" > $OUTPUT_DIR/negative_features.tsv
+  ) TO STDOUT;" >> $OUTPUT_DIR/negative_features.tsv
 
 fi
